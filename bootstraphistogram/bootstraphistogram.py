183c0d4f2acabd94f6df77ea8cb19596454aa81a
David Hadley <d.r.hadley@warwick.ac.uk>
2020-06-25 14:47:59 +0100

    basic implementation and tests.

--- bootstraphistogram/bootstraphistogram.py
+++ bootstraphistogram/bootstraphistogram.py
@@ -1,7 +1,33 @@
-from typing import Any
+from ctypes import Array
+from typing import Any, Union, List, Tuple, Optional
 
 import boost_histogram as bh
+import numpy as np
+
 
 class BootstrapHistogram:
-    def __init__(self, *axes: bh.axis.Axis, numbootstrapsamples: int=1000, **kwargs: Any):
+    def __init__(self, *axes: bh.axis.Axis, numbootstrapsamples: int = 1000, **kwargs: Any):
+        axes = list(axes)
+        axes.append(bh.axis.Integer(0, numbootstrapsamples))
         self._hist = bh.Histogram(*axes, **kwargs)
+
+    @property
+    def numbootstrapsamples(self) -> int:
+        return len(self.axes[-1])
+
+    @property
+    def axes(self) -> Tuple[bh.axis.Axis, ...]:
+        return self._hist.axes
+
+    def fill(self, *args: np.ndarray,
+             weight: Optional[np.ndarray] = None,
+             **kwargs: Any) -> "BootstrapHistogram":
+
+        for i in range(self.numbootstrapsamples):
+            w = np.random.poisson(1.0, size=args[0].shape)
+            ax = list(args) + [i]
+            self._hist.fill(*ax, weight=w, **kwargs)
+        return self
+
+    def view(self, flow=False) -> Any:
+        return self._hist.view(flow=flow)
--- tests/test_bootstraphistogram.py
+++ tests/test_bootstraphistogram.py
@@ -1,9 +1,46 @@
 import unittest
 import boost_histogram as bh
+import numpy as np
+
 from bootstraphistogram import BootstrapHistogram
 
+
+def _standard_error_mean(size, sigma=1.0):
+    return sigma / np.sqrt(size)
+
+def _standard_error_std(size, sigma=1.0):
+    return np.sqrt(sigma**2/(2.0*size))
 
 class TestBootstrapHistogram1D(unittest.TestCase):
     def test_contructor(self):
         # check constructor works without raising error
         BootstrapHistogram(bh.axis.Regular(100, -1.0, 1.0))
+
+    def test_fill(self):
+        hist = BootstrapHistogram(bh.axis.Regular(100, -5.0, 5.0))
+        size = 100000
+        data = np.random.normal(loc=0.0, scale=1.0,  size=size)
+        hist.fill(data)
+        x = hist.axes[0].centers
+        y = hist.view()[:,0]
+        mean = np.average(x, weights=y)
+        std = np.average((x - mean)**2, weights=y)
+        self.assertAlmostEqual(mean, 0.0, delta=5.0*_standard_error_mean(size=size))
+        self.assertAlmostEqual(std, 1.0, delta=5.0*_standard_error_std(size=size))
+        return
+
+    def test_numbootstrapsamples_property(self):
+        numbootstrapsamples = 100
+        hist = BootstrapHistogram(bh.axis.Regular(100, -5.0, 5.0), numbootstrapsamples=numbootstrapsamples)
+        self.assertEqual(hist.numbootstrapsamples, numbootstrapsamples)
+
+    def test_axes_property(self):
+        axes = (bh.axis.Regular(100, -5.0, 5.0),)
+        hist = BootstrapHistogram(*axes)
+        self.assertEqual(hist.axes[:-1], axes)
+
+    def test_view_property(self):
+        numbootstrapsamples = 100
+        hist = BootstrapHistogram(bh.axis.Regular(100, -5.0, 5.0), numbootstrapsamples=numbootstrapsamples)
+        view = hist.view()
+        self.assertEqual(view)
