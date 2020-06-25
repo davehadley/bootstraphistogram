ed0224111f718f0c132dcf9ef5b80ff1d80441ef
David Hadley <d.r.hadley@warwick.ac.uk>
2020-06-25 18:50:22 +0100

    Test sample distributions.

--- tests/test_bootstraphistogram.py
+++ tests/test_bootstraphistogram.py
@@ -1,4 +1,6 @@
 import unittest
+from typing import Optional
+
 import boost_histogram as bh
 import numpy as np
 
@@ -8,25 +10,47 @@ from bootstraphistogram import BootstrapHistogram
 def _standard_error_mean(size, sigma=1.0):
     return sigma / np.sqrt(size)
 
+
 def _standard_error_std(size, sigma=1.0):
-    return np.sqrt(sigma**2/(2.0*size))
+    return np.sqrt(sigma ** 2 / (2.0 * size))
+
 
 class TestBootstrapHistogram1D(unittest.TestCase):
+
+    def assertArrayAlmostEqual(self, actual: np.ndarray, expected: np.ndarray, delta: float, msg: Optional[str] = None):
+        return self.assertTrue(np.all(np.abs(actual - expected) < delta), msg=msg)
+
     def test_contructor(self):
         # check constructor works without raising error
         BootstrapHistogram(bh.axis.Regular(100, -1.0, 1.0))
+        return
 
     def test_fill(self):
-        hist = BootstrapHistogram(bh.axis.Regular(100, -5.0, 5.0))
+        hist = BootstrapHistogram(bh.axis.Regular(100, -5.0, 5.0), numbootstrapsamples=10)
         size = 100000
-        data = np.random.normal(loc=0.0, scale=1.0,  size=size)
+        data = np.random.normal(loc=0.0, scale=1.0, size=size)
         hist.fill(data)
         x = hist.axes[0].centers
-        y = hist.view()[:,np.random.randint(0, hist.numbootstrapsamples)]
+        y = hist.view()[:, np.random.randint(0, hist.numbootstrapsamples)]
         mean = np.average(x, weights=y)
-        std = np.average((x - mean)**2, weights=y)
-        self.assertAlmostEqual(mean, 0.0, delta=5.0*_standard_error_mean(size=size))
-        self.assertAlmostEqual(std, 1.0, delta=5.0*_standard_error_std(size=size))
+        std = np.average((x - mean) ** 2, weights=y)
+        binwidth = hist.axes[0].edges[1] - hist.axes[0].edges[0]
+        self.assertAlmostEqual(mean, 0.0, delta=5.0 * _standard_error_mean(size=size) + binwidth)
+        self.assertAlmostEqual(std, 1.0, delta=5.0 * _standard_error_std(size=size) + binwidth)
+        return
+
+    def test_samples(self):
+        numbootstrapsamples = 100
+        hist = BootstrapHistogram(bh.axis.Regular(100, 0.0, 1.0), numbootstrapsamples=numbootstrapsamples)
+        size = 100000
+        data = np.random.uniform(size=size)
+        hist.fill(data)
+        y = hist.view()
+        mean = np.average(y, axis=1)
+        std = np.std(y, axis=1)
+        nbins = len(hist.axes[0])
+        self.assertArrayAlmostEqual(mean, size / nbins, delta=5.0 * np.sqrt(size/nbins))
+        self.assertArrayAlmostEqual(std, np.sqrt(size / nbins), delta=5.0 * _standard_error_std(size=numbootstrapsamples, sigma=np.sqrt(size/nbins)))
         return
 
     def test_numbootstrapsamples_property(self):
---
+++ tests/test_standarderror.py
@@ -0,0 +1,44 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def _standard_error_mean(size, sigma=1.0):
+    return sigma / np.sqrt(size)
+
+
+def _mc_error_mean(size, sigma=1.0, nmc=1000):
+    return np.std([np.average(np.random.normal(size=size, scale=sigma)) - 0.0 for _ in range(nmc)])
+
+
+def _standard_error_std(size, sigma=1.0):
+    return np.sqrt(sigma ** 2 / (2.0 * size))
+
+
+def _mc_error_std(size, sigma=1.0, nmc=1000):
+    return np.std([np.std(np.random.normal(size=size, scale=sigma)) - sigma for _ in range(nmc)])
+
+
+def main():
+    fig = plt.figure()
+    ax = fig.add_subplot(211)
+    X = np.arange(10, 100, 10)
+    sigma = 2.0
+    # check mean
+    Y = [_mc_error_mean(size=x, sigma=sigma) for x in X]
+    ax.plot(X, Y, label="MC")
+    Y = [_standard_error_mean(size=x, sigma=sigma) for x in X]
+    ax.plot(X, Y, label="Analytic")
+    ax.legend()
+    # check std dev
+    ax = fig.add_subplot(212)
+    Y = [_mc_error_std(size=x, sigma=sigma) for x in X]
+    ax.plot(X, Y, label="MC")
+    Y = [_standard_error_std(size=x, sigma=sigma) for x in X]
+    ax.plot(X, Y, label="Analytic")
+    ax.legend()
+    plt.show()
+    return
+
+
+if __name__ == "__main__":
+    main()
