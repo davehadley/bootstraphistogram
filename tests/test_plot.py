7ca64d0e4cde85e20dde61ce4dd846f83833851a
David Hadley <d.r.hadley@warwick.ac.uk>
2020-06-25 22:05:26 +0100

    Test variable binning plot.

--- tests/test_plot.py
+++ tests/test_plot.py
@@ -8,13 +8,13 @@ from bootstraphistogram import BootstrapHistogram, axis, plot
 
 class TestPlotBootstrapHistogram1D(unittest.TestCase):
 
-    def normalhist(self, size=100000, nbins=100, numsamples=10, mu=0.0, sigma=1.0, low=-5.0, high=5.0):
-        hist = BootstrapHistogram(axis.Regular(nbins, low, high), numsamples=numsamples)
+    def normalhist(self, ax=axis.Regular(100, -5.0, 5.0), size=100000, numsamples=10, mu=0.0, sigma=1.0):
+        hist = BootstrapHistogram(ax, numsamples=numsamples)
         hist.fill(np.random.normal(loc=mu, scale=sigma, size=size))
         return hist
 
     def test_plot_errorbar(self):
-        hist = self.normalhist(nbins=10, size=100)
+        hist = self.normalhist(ax=axis.Variable([-5.0, -2.5, 0.0, 1.0, 2.0, 3.0, 4.0, 5.0]), size=100)
         plot.errorbar(hist)
         plt.show()
         return
