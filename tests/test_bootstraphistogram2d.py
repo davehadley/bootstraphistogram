85a0d8268f4dec7ca3a209034db08028d1800fd5
David Hadley <d.r.hadley@warwick.ac.uk>
2020-06-26 16:53:44 +0100

    Add test for 2D filling.

---
+++ tests/test_bootstraphistogram2d.py
@@ -0,0 +1,47 @@
+import pickle
+import unittest
+from typing import Optional
+
+import boost_histogram as bh
+import numpy as np
+
+from bootstraphistogram import BootstrapHistogram
+
+
+def _standard_error_mean(size, sigma=1.0):
+    return sigma / np.sqrt(size)
+
+
+def _standard_error_std(size, sigma=1.0):
+    return np.sqrt(sigma ** 2 / (2.0 * size))
+
+class TestBootstrapHistogram1D(unittest.TestCase):
+    def assertArrayEqual(self, actual: np.ndarray, expected: np.ndarray, msg: Optional[str] = None) -> None:
+        return self.assertTrue(np.array_equal(actual, expected), msg=msg)
+
+    def assertArrayAlmostEqual(self, actual: np.ndarray, expected: np.ndarray, delta: float,
+                               msg: Optional[str] = None) -> None:
+        return self.assertTrue(np.all(np.abs(actual - expected) < delta), msg=msg)
+
+    def test_contructor(self):
+        # check constructor works without raising error
+        BootstrapHistogram(bh.axis.Regular(100, -1.0, 1.0), bh.axis.Variable([0.0, 1.0, 3.0, 6.0]))
+        return
+
+    def test_fill(self):
+        hist = BootstrapHistogram(bh.axis.Regular(100, -5.0, 5.0), bh.axis.Regular(100, -5.0, 5.0), numsamples=10)
+        size = 100000
+        x = np.random.normal(loc=0.0, scale=1.0, size=size)
+        y = np.random.normal(loc=0.0, scale=1.0, size=size)
+        hist.fill(x, y)
+        y = hist.view()[:, :, np.random.randint(0, hist.numsamples)]
+        binwidth = hist.axes[0].edges[1] - hist.axes[0].edges[0]
+        mean = np.average(hist.axes[0].centers, weights=np.sum(y, axis=1))
+        std = np.average((hist.axes[0].centers - mean) ** 2, weights=np.sum(y, axis=1))
+        self.assertAlmostEqual(mean, 0.0, delta=5.0 * _standard_error_mean(size=size) + binwidth)
+        self.assertAlmostEqual(std, 1.0, delta=5.0 * _standard_error_std(size=size) + binwidth)
+        mean = np.average(hist.axes[1].centers, weights=np.sum(y, axis=0))
+        std = np.average((hist.axes[1].centers - mean) ** 2, weights=np.sum(y, axis=0))
+        self.assertAlmostEqual(mean, 0.0, delta=5.0 * _standard_error_mean(size=size) + binwidth)
+        self.assertAlmostEqual(std, 1.0, delta=5.0 * _standard_error_std(size=size) + binwidth)
+        return
