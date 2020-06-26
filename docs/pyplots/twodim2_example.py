cf61a38cddb0ce4df5cac508c90dc254ec1ba90a
David Hadley <d.r.hadley@warwick.ac.uk>
2020-06-26 19:58:31 +0100

    add alternative simple example.

--- docs/pyplots/twodim2_example.py
+++ docs/pyplots/twodim2_example.py
@@ -4,7 +4,20 @@ import matplotlib.pyplot as plt
 from bootstraphistogram import BootstrapHistogram, axis
 
 @np.vectorize
-def stddev_bootstrap(nevents=100, numbins=2, numsamples=100):
+def stddev_bootstrap_1d(nevents=100, numbins=2, numsamples=100):
+    # create a 2D histogram
+    histX = BootstrapHistogram(axis.Regular(numbins, 0.0, 1.0), numsamples=numsamples, rng=1234)
+    histY = BootstrapHistogram(axis.Regular(numbins, 0.0, 1.0), numsamples=numsamples, rng=1234)
+    # fill with some random data
+    X = np.random.uniform(size=nevents)
+    Y = np.random.uniform(size=nevents)
+    histX = histX.fill(X)
+    histY = histY.fill(Y)
+    histSum = histX + histY
+    return histSum.std()[0]
+
+@np.vectorize
+def stddev_bootstrap_2d(nevents=100, numbins=2, numsamples=100):
     # create a 2D histogram
     hist2d = BootstrapHistogram(axis.Regular(numbins, 0.0, 1.0), axis.Regular(numbins, 0.0, 1.0), numsamples=numsamples)
     # fill with some random data
@@ -13,6 +26,7 @@ def stddev_bootstrap(nevents=100, numbins=2, numsamples=100):
     histY = hist2d.project(1)
     histSum = histX + histY
     return histSum.std()[0]
+
 
 @np.vectorize
 def stddev_analytic(nevents=100, numbins=2):
@@ -24,7 +38,8 @@ def stddev_analytic(nevents=100, numbins=2):
 def example2d():
     N = list(range(10, 1000, 10))
     plt.plot(N, stddev_analytic(N), label=r"$\sigma$ analytic")
-    plt.plot(N, stddev_bootstrap(N), label=r"$\sigma$ bootstrap")
+    plt.plot(N, stddev_bootstrap_1d(N), label=r"$\sigma$ bootstrap 1D")
+    plt.plot(N, stddev_bootstrap_2d(N), label=r"$\sigma$ bootstrap 2D")
     plt.xlabel("N")
     plt.legend()
     plt.show()
