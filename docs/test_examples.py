67b8d361ee5d8eeba288d3b5716f391f2fadd34c
David Hadley <davehadley@users.noreply.github.com>
2020-11-07 12:56:10 +0000

    Add a test that runs the example files in the documentation.

---
+++ docs/test_examples.py
@@ -0,0 +1,19 @@
+import glob
+import os
+from subprocess import check_call
+
+import pytest
+
+
+def _getexamplefiles():
+    dirname = os.path.dirname(os.path.abspath(__file__))
+    return [
+        fname
+        for fname in glob.glob(os.sep.join((dirname, "*", "*.py")))
+        if __file__ not in fname
+    ]
+
+
+@pytest.mark.parametrize("examplefile", _getexamplefiles())
+def test_example(examplefile):
+    check_call(["python", examplefile])
