Improve documentation. Add new tests. Fix shape bug in skewness calculation.

src/bootstraphistogram/bootstraphistogram.py
@@ -51,17 +51,21 @@ class BootstrapHistogram:
         axeslist.append(bh.axis.Integer(0, numsamples))
         self._random = np.random.default_rng(rng)
         self._hist = bh.Histogram(*axeslist, **kwargs)
+        # when filling with very large arrays, the fast filling method may use too
+        # much memory, fall back to the slower method when the array size gets above
+        # this threshold
+        self._threshold_for_fast_method = 1000000
 
     @property
     def nominal(self) -> bh.Histogram:
-        """boost_histogram.Histogram: A histogram of the filled values, with no
+        """A histogram of the filled values, with no
         bootstrap samples.
         """
         return self._nominal
 
     @property
     def samples(self) -> bh.Histogram:
-        """boost_histogram.Histogram: A histogram of the bootstrap samples. The last
+        """A histogram of the bootstrap samples. The last
         dimension corresponds to the bootstrap sample index and is of size
         :py:attr:`BootstrapHistogram.numsamples`.
         """
@@ -116,12 +120,12 @@ class BootstrapHistogram:
 
     @property
     def numsamples(self) -> int:
-        """int: Number of bootstrap re-samplings."""
+        """Number of bootstrap re-samplings."""
         return len(self.axes[-1])
 
     @property
     def axes(self) -> Tuple[bh.axis.Axis, ...]:
-        """Tuple[bh.axis.Axis, ...]: :py:class:`boost_histogram.axis.Axis` representing
+        """:py:class:`boost_histogram.axis.Axis` representing
         the histogram binning. The last dimension corresponds to the bootstrap sample
         index."""
         return self._hist.axes
@@ -162,7 +166,7 @@ class BootstrapHistogram:
         weight = np.asarray(weight) if weight is not None else None
         seed = np.asarray(seed) if seed is not None else None
         self._validate_fill_inputs(args, weight, seed)
-        if self.numsamples * args[0].size < 1000000:
+        if (self.numsamples * args[0].size) < self._threshold_for_fast_method:
             return self._fill_fast(*args, weight=weight, seed=seed, **kwargs)
         else:
             return self._fill_slow(*args, weight=weight, seed=seed, **kwargs)

src/bootstraphistogram/bootstrapmoment.py
@@ -28,10 +28,46 @@ class BootstrapMoment:
     rng : Union[int, np.random.Generator, None]
         A numpy generator. If not provided, the numpy default from
         :py:func:`numpy.random.default_rng` will be used.
+    **kwargs: Any
+        Passed on to the underlying :py:class:`bootstraphistogram.BootstrapHistogram`.
+
+    Examples
+    --------
+
+    To create and fill an instance:
+
+    >>> from bootstraphistogram import BootstrapMoment
+    >>> import numpy as np
+    >>> moments = BootstrapMoment(3, rng=1234)
+    >>> moments.fill(np.arange(101))
+
+    Compute the mean:
+
+    >>> moments.mean().nominal
+    50.0
+    >>> moments.mean().samples
+    array([51.4, 50.3, 46.1 ])
+
+    Compute the standard deviation:
+
+    >>> moments.std().nominal
+    29.15
+    >>> moments.std().samples
+    array([29.5, 29.5, 28.6])
+
+    Compute the skewness:
+
+    >>> moments.skewness().nominal
+    0.0
+    >>> moments.skewness().samples
+    array([-0.00,  0.10,  0.06])
     """
 
     def __init__(
-        self, numsamples: int = 1000, rng: Union[int, np.random.Generator, None] = None
+        self,
+        numsamples: int = 1000,
+        rng: Union[int, np.random.Generator, None] = None,
+        **kwargs: Any,
     ):
         ax = bh.axis.Regular(1, -1.0, 1.0)
         # provide identical generator to each histogram to ensure that sample weights
@@ -39,10 +75,18 @@ class BootstrapMoment:
         if rng is None:
             rng = int(np.random.default_rng(rng).integers(np.iinfo(int).max))
         # we must deepcopy the rng in case it is a Generator with some internal state
-        self._sum_w = BootstrapHistogram(ax, numsamples=numsamples, rng=deepcopy(rng))
-        self._sum_wt = BootstrapHistogram(ax, numsamples=numsamples, rng=deepcopy(rng))
-        self._sum_wt2 = BootstrapHistogram(ax, numsamples=numsamples, rng=deepcopy(rng))
-        self._sum_wt3 = BootstrapHistogram(ax, numsamples=numsamples, rng=deepcopy(rng))
+        self._sum_w = BootstrapHistogram(
+            ax, numsamples=numsamples, rng=deepcopy(rng), **kwargs
+        )
+        self._sum_wt = BootstrapHistogram(
+            ax, numsamples=numsamples, rng=deepcopy(rng), **kwargs
+        )
+        self._sum_wt2 = BootstrapHistogram(
+            ax, numsamples=numsamples, rng=deepcopy(rng), **kwargs
+        )
+        self._sum_wt3 = BootstrapHistogram(
+            ax, numsamples=numsamples, rng=deepcopy(rng), **kwargs
+        )
 
     def __eq__(self, other: object) -> bool:
         return (
@@ -168,22 +212,22 @@ class BootstrapMoment:
             sumwt2=self._sum_wt2.samples.view(),
             sumwt3=self._sum_wt3.samples.view(),
         )
-        return ValueWithSamples(nominal, samples)
+        return ValueWithSamples(nominal, samples.flatten())
 
     @property
     def numsamples(self) -> int:
-        """int: Number of bootstrap re-samplings."""
+        """Number of bootstrap re-samplings."""
         return self._sum_w.numsamples
 
 
 def _mean(sumw: ArrayLike, sumwt: ArrayLike) -> ArrayLike:
-    return sumwt / sumw
+    return np.divide(sumwt, sumw)
 
 
 def _variance(sumw: ArrayLike, sumwt: ArrayLike, sumwt2: ArrayLike) -> ArrayLike:
     mu = _mean(sumwt=sumwt, sumw=sumw)
-    mu2 = mu * mu
-    return mu2 + ((sumwt2 - 2.0 * sumwt * mu) / sumw)
+    mu2 = np.multiply(mu, mu)
+    return mu2 + np.divide((sumwt2 - 2.0 * np.multiply(sumwt, mu)), sumw)
 
 
 def _skewness(
@@ -191,5 +235,8 @@ def _skewness(
 ) -> ArrayLike:
     mu = _mean(sumwt=sumwt, sumw=sumw)
     sigma = np.sqrt(_variance(sumw=sumw, sumwt=sumwt, sumwt2=sumwt2))
-    mut3 = sumwt3 / sumw
-    return (mut3 - 3 * mu * np.power(sigma, 2) - np.power(mu, 3)) / np.power(sigma, 3)
+    mut3 = np.divide(sumwt3, sumw)
+    return np.divide(
+        (mut3 - 3 * np.multiply(mu, np.power(sigma, 2)) - np.power(mu, 3)),
+        np.power(sigma, 3),
+    )

src/bootstraphistogram/valuewithsamples.py
@@ -23,12 +23,12 @@ class ValueWithSamples(Generic[T]):
 
     @property
     def nominal(self) -> T:
-        """T: the value without any resampling."""
+        """the value without any resampling."""
         return self._nominal
 
     @property
     def samples(self) -> np.ndarray:
-        """np.ndarray: the value with Poisson bootstrap resampling applied."""
+        """the value with Poisson bootstrap resampling applied."""
         return self._samples
 
     def __eq__(self, other: object) -> bool: